116a90c39cf2cf5291ec24854dc5f6c97872a158
Elliot Berdy <berdyelliot@gmail.com>
2023-10-17 21:26:23 -0700

    removed icon

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -19,10 +19,10 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         {/* several domains list the same content, make sure google knows we mean this one. */}
         <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
 
-        <link href="/favicon.ico" rel="icon" sizes="any" />
+        {/* <link href="/favicon.ico" rel="icon" sizes="any" />
         <link href="/icon.svg" rel="icon" type="image/svg+xml" />
         <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
-        <link href="/site.webmanifest" rel="manifest" />
+        <link href="/site.webmanifest" rel="manifest" /> */}
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
